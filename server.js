cf92283a29a9a72309e5af7d33619f0f8b185c82
fardin654 <fardinmd654@gmail.com>
2025-08-20 20:05:55 +0530

    server.js changed

--- server.js
+++ server.js
@@ -1,37 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const app = express();
+const app = require('./api/index');
 
-// Middleware
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
-
-// Routes
-console.log('Setting up routes');
-app.use('/api/members', require('./routes/members'));
-app.use('/api/expenses', require('./routes/expenses'));
-app.use('/api/rounds', require('./routes/rounds'));
-app.use('/api/wallets', require('./routes/wallet'));
-app.use('/api/payments', require('./routes/payments'));
-
-// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
